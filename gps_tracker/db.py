82e8c388266d4969159b85f89451d3d69e81fd41
crucl0 <alekz.polyakov@gmail.com>
2014-02-06 18:33:43 +0200

    move "db_url" to settings

--- gps_tracker/db.py
+++ gps_tracker/db.py
@@ -5,12 +5,12 @@ import pymongo
 def includeme(request):
     settings = request.registry.settings
     db_url = urlparse(settings['mongo_uri'])
+    settings['db_url'] = db_url
     connection = pymongo.MongoClient(host=db_url.hostname, port=db_url.port)
     request.registry.mongo_connection = connection
 
 
 def db_connection(request):
-    settings = request.registry.settings
-    db_url = urlparse(settings['mongo_uri'])
+    db_url = request.registry.settings['db_url']
     db = request.registry.mongo_connection[db_url.path[1:]]
     return db
