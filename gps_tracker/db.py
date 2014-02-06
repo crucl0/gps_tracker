f90b3deb930af628727fa732b2b1ef9d42c25c1f
crucl0 <alekz.polyakov@gmail.com>
2014-02-06 15:52:48 +0200

    add DB-tests, rewrite db_connection(), move all *.ini files to settings/ directory

--- gps_tracker/db.py
+++ gps_tracker/db.py
@@ -5,7 +5,7 @@ import pymongo
 def db_connection(request):
         settings = request.registry.settings
         db_url = urlparse(settings['mongo_uri'])
-        conn = pymongo.Connection(host=db_url.hostname, port=db_url.port)
-        settings['db_conn'] = conn
-        db = settings['db_conn'][db_url.path[1:]]
+        connection = pymongo.MongoClient(host=db_url.hostname, port=db_url.port)
+        request.registry.mongo_connect = connection
+        db = request.registry.mongo_connect[db_url.path[1:]]
         return db
--- gps_tracker/tests.py
+++ gps_tracker/tests.py
@@ -1,6 +1,8 @@
 import unittest
-
 from pyramid import testing
+from pyramid.paster import get_appsettings
+from .views import main_view
+from .db import db_connection
 
 
 class ViewTests(unittest.TestCase):
@@ -11,7 +13,28 @@ class ViewTests(unittest.TestCase):
         testing.tearDown()
 
     def test_main_view(self):
-        from .views import main_view
         request = testing.DummyRequest()
         info = main_view(request)
         self.assertEqual(info['project'], 'gps_tracker')
+
+
+class DB_Tests(unittest.TestCase):
+    def setUp(self):
+        settings = get_appsettings('settings/test.ini')
+        self.config = testing.setUp(settings=settings)
+        request = testing.DummyRequest()
+        self.db = db_connection(request)
+        connection = request.registry.mongo_connect
+        connection.drop_database('db_for_test')
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_db_connection(self):
+        # test CRUD
+        collection = self.db.test_collection
+        test_data = {"a": 1, "b": 2, "c": 3}
+        collection.save(test_data)
+        read_back = collection.find_one()
+        self.assertTrue(type(read_back), dict)
+        self.assertIn("b", read_back)
