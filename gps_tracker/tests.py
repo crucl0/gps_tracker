531d163bfc531009ce7ab973f7677a4a9491c3ac
crucl0 <alekz.polyakov@gmail.com>
2014-02-09 16:54:54 +0200

    delete view's tests

--- gps_tracker/tests.py
+++ gps_tracker/tests.py
@@ -1,21 +1,7 @@
 import unittest
 from pyramid import testing
 from pyramid.paster import get_appsettings
-from .views import main_view
 from .db import db_connection
-
-
-class ViewTests(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp()
-
-    def tearDown(self):
-        testing.tearDown()
-
-    def test_main_view(self):
-        request = testing.DummyRequest()
-        info = main_view(request)
-        self.assertEqual(info['project'], 'gps_tracker')
 
 
 class DB_Tests(unittest.TestCase):
@@ -24,7 +10,6 @@ class DB_Tests(unittest.TestCase):
         self.config = testing.setUp(settings=settings)
         self.config.include('gps_tracker.db')
         self.request = testing.DummyRequest()
-
         connection = self.request.registry.mongo_connection
         connection.drop_database('db_for_test')
 
@@ -32,7 +17,6 @@ class DB_Tests(unittest.TestCase):
         testing.tearDown()
 
     def test_db_connection(self):
-        # test CRUD
         db = db_connection(self.request)
         collection = db.test_collection
         test_data = {"a": 1, "b": 2, "c": 3}
