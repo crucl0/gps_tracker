75d891103467ae5e61286b77a1b877f502fec767
crucl0 <alekz.polyakov@gmail.com>
2014-02-12 17:53:08 +0200

    filled in views for requests

--- gps_tracker/views.py
+++ gps_tracker/views.py
@@ -1,44 +1,86 @@
 from pyramid.view import view_config
-
-points_list = [
-    {"_id": 'ObjectId("52e3eb56a7cade5d0898e012")', "latitude": "45.215",
-     "longitude": "14.131", "gas_station": "Lukoil", "odometer": "24100",
-     "description": "Bad coffee"},
-    {"_id": 'ObjectId("52e3eb79a7cade5d0898e013")', "latitude": "47.412",
-     "longitude": "16.112", "gas_station": "Shell", "odometer": "24300",
-     "description": "Nice service, but fuel is more expensive"},
-    {"_id": 'ObjectId("52e3eba5a7cade5d0898e014")', "latitude": "48.544",
-     "longitude": "17.001", "gas_station": "Руснефть", "odometer": "24500",
-     "description": "На заправке есть гостиница и кафе. Очень хорошо"},
-    {"_id": 'ObjectId("52e3ec19a7cade5d0898e015")', "latitude": "49.165",
-     "longitude": "18.125", "gas_station": "Татнефть", "odometer": "24750",
-     "description": "Есть стоянка кемпинг-стоянка. Дешёвая незамерзайка."},
-    {"_id": 'ObjectId("52f3aaf0a7cade0d846d00d7")', "gas_station": "Газпром",
-     "odometer": "28400", "latitude": "49.249",
-     "description": "Хорошее кафе, есть душ!", "longitude": "19.100"}
-]
+from bson.objectid import ObjectId
+from bson.json_util import dumps
+from bson.errors import InvalidId
 
 
 @view_config(route_name='points', request_method='GET', renderer='json')
 def points_get_all(request):
-    return points_list
+    points = dumps([point for point in request.mongo_db.points.find()])
+    return points
 
 
 @view_config(route_name='points', request_method='POST', renderer='json')
 def point_add_new(request):
-    return points_list[2]
+    try:
+        if len(request.json_body) < 4:
+            return {"Error": "Expects at least 3 parameters about new point"}
+        else:
+            point = {key: request.json_body[key] for key in request.json_body}
+            point_new_id = request.mongo_db.points.insert(point)
+            return dumps(request.mongo_db.points.find_one(
+                {"_id": ObjectId(point_new_id)}))
+    except ValueError:
+        return {"Error": "POST request body must be JSON"}
 
 
 @view_config(route_name='point', request_method='GET', renderer='json')
 def point_get_one(request):
-    return points_list[0]
+    try:
+        _id = ObjectId(request.matchdict["id"])
+        point = request.mongo_db.points.find_one({"_id": _id})
+        return dumps(point)
+    except InvalidId:
+        return {"Error": "Invalid ObjectId. Check your request."}
 
 
 @view_config(route_name='point', request_method='PATCH', renderer='json')
 def point_edit_one(request):
-    return points_list[1]
+    try:
+        try:
+            _id = ObjectId(request.matchdict["id"])
+            request.mongo_db.points.find_one(_id)
+        except InvalidId:
+            return {"Error": "Invalid ObjectId. Check your request."}
+
+        if len(request.json_body) == 0:
+            return {"Error": "PATCH request body if empty. Nothing to do."}
+        else:
+            _id = ObjectId(request.matchdict["id"])
+            updates = request.json_body
+            request.mongo_db.points.update({"_id": _id},
+                                           {"$set": updates})
+            return dumps(request.mongo_db.points.find_one(_id))
+    except ValueError:
+        return {"Error": "PATCH request body must be JSON"}
+
+
+@view_config(route_name='point', request_method='PUT', renderer='json')
+def point_update_one(request):
+    try:
+        try:
+            _id = ObjectId(request.matchdict["id"])
+            request.mongo_db.points.find_one(_id)
+        except InvalidId:
+            return {"Error": "Invalid ObjectId. Check your request."}
+
+        if len(request.json_body) == 0:
+            return {"Error": "PUT request body if empty. Nothing to do."}
+        else:
+            _id = ObjectId(request.matchdict["id"])
+            replacement = request.json_body
+            replacement["_id"] = _id
+            request.mongo_db.points.save(replacement)
+            return dumps(request.mongo_db.points.find_one(_id))
+    except ValueError:
+        return {"Error": "PUT request body must be JSON"}
 
 
 @view_config(route_name='point', request_method='DELETE', renderer='json')
 def point_delete_one(request):
-    return {}
+    try:
+        _id = ObjectId(request.matchdict["id"])
+        request.mongo_db.points.remove({"_id": _id})
+        return {}
+    except InvalidId:
+        return {"Error": "Invalid ObjectId. Check your request."}
