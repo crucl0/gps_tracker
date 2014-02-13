93063150de46302058b9f03975bcd0410fc1a62d
crucl0 <alekz.polyakov@gmail.com>
2014-02-13 20:40:10 +0200

    rewrite includeme

--- gps_tracker/__init__.py
+++ gps_tracker/__init__.py
@@ -7,12 +7,10 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.include('gps_tracker.db')
-    config.include('gps_tracker.db.json_mongo_adapters')
     config.add_static_view('static', 'static', cache_max_age=3600)
 
     config.add_route('points', '/points')
     config.add_route('point', '/points/{id}')
-
     config.add_request_method(mongo_db_connection, 'mongo_db', reify=True)
     config.scan()
     return config.make_wsgi_app()
--- gps_tracker/db.py
+++ gps_tracker/db.py
@@ -5,8 +5,8 @@ import datetime
 import pymongo
 
 
-def includeme(request):
-    settings = request.registry.settings
+def includeme(config):
+    settings = config.get_settings()
     mongo_db_url = urlparse(settings['mongo_uri'])
     settings['mongo_db_url'] = mongo_db_url
     settings['mongo_db_name'] = mongo_db_url.path[1:]
@@ -14,7 +14,12 @@ def includeme(request):
     settings['mongo_port'] = int(mongo_db_url.port or 27017)
     connection = pymongo.MongoClient(host=mongo_db_url.hostname,
                                      port=mongo_db_url.port)
-    request.registry.mongo_connection = connection
+    config.registry.mongo_connection = connection
+    json_mongo_renderer = JSON()
+    json_mongo_renderer.add_adapter(ObjectId, bson_adapter)
+    json_mongo_renderer.add_adapter(datetime.datetime, datetime_adapter)
+    json_mongo_renderer.add_adapter(pymongo.cursor.Cursor, json_cursor_adapter)
+    config.add_renderer('json', json_mongo_renderer)
 
 
 def mongo_db_connection(request):
@@ -33,11 +38,3 @@ def datetime_adapter(obj, request):
 
 def json_cursor_adapter(obj, request):
     return list(obj)
-
-
-def json_mongo_adapters(config):
-    json_mongo_renderer = JSON()
-    json_mongo_renderer.add_adapter(ObjectId, bson_adapter)
-    json_mongo_renderer.add_adapter(datetime.datetime, datetime_adapter)
-    json_mongo_renderer.add_adapter(pymongo.cursor.Cursor, json_cursor_adapter)
-    config.add_renderer('json', json_mongo_renderer)
