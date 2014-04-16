35d2c69dc5b06d883faccf26541e72597797c482
crucl0 <alekz.polyakov@gmail.com>
2014-04-17 00:37:54 +0300

    RESTView is started

--- gps_tracker/__init__.py
+++ gps_tracker/__init__.py
@@ -1,5 +1,9 @@
 from pyramid.config import Configurator
 from .db import mongo_db_connection
+# from .views import (Points, Companies)
+from .views import RESTView
+
+resources = ['Points', 'Companies', 'Stations']
 
 
 def main(global_config, **settings):
@@ -10,8 +14,23 @@ def main(global_config, **settings):
     config.add_static_view('static', 'static', cache_max_age=3600)
 
     config.add_route('points', '/points')
+    config.add_route('companies', '/companies')
     config.add_route('point', '/points/{id}')
+
     config.add_static_view('/', 'gps_tracker:templates/', cache_max_age=0)
+    config.add_view(
+        RESTView, route_name='points', attr='get_all',
+        request_method='GET', renderer='json')
+    config.add_view(
+        RESTView, route_name='point', attr='get_one',
+        request_method='GET', renderer='json')
+    config.add_view(
+        RESTView, route_name='points', attr='add_new',
+        request_method='POST', renderer='json')
+
+    config.add_view(
+        RESTView, route_name='companies', attr='get_all',
+        request_method='GET', renderer='json')
 
     config.add_request_method(mongo_db_connection, 'mongo_db', reify=True)
     config.scan()
--- gps_tracker/views.py
+++ gps_tracker/views.py
@@ -6,70 +6,101 @@ from pyramid.httpexceptions import (HTTPNotFound,
                                     HTTPBadRequest)
 
 
-@view_config(route_name='points', request_method='GET', renderer='json')
-def points_get_all(request):
-    return list(request.mongo_db.points.find())
+class RESTView(object):
+    def __init__(self, request):
+        self.request = request
+        self.resource = self.request.path_info_peek()
+
+    def get_all(self):
+        return getattr(self.request.mongo_db, self.resource).find()
+
+    def get_one(self):
+        try:
+            _id = ObjectId(self.request.matchdict["id"])
+            return getattr(self.request.mongo_db, self.resource).find_one(_id)
+        except InvalidId:
+            return HTTPNotFound("There is no such object")
+
+    def add_new(self):
+        item_new = {key: self.request.json_body[key] for key in self.request.json_body}
+        item_new_id = getattr(self.request.mongo_db, self.resource).insert(item_new)
+        return self.request.mongo_db.points.find_one(item_new_id)
+
+    def put(self):
+        pass
+
+    def patch(self):
+        pass
+
+    def delete(self):
+        pass
 
 
-@view_config(route_name='points', request_method='POST', renderer='json')
-def point_add_new(request):
-    point = {key: request.json_body[key] for key in request.json_body}
-    point_new_id = request.mongo_db.points.insert(point)
-    return request.mongo_db.points.find_one(point_new_id)
+
+# @view_config(route_name='points', request_method='GET', renderer='json')
+# def points_get_all(request):
+#     return list(request.mongo_db.points.find())
 
 
-@view_config(route_name='point', request_method='GET', renderer='json')
-def point_get_one(request):
-    try:
-        _id = ObjectId(request.matchdict["id"])
-        point = request.mongo_db.points.find_one(_id)
-        return point
-    except InvalidId:
-        return HTTPNotFound("There is no such object")
+# @view_config(route_name='points', request_method='POST', renderer='json')
+# def point_add_new(request):
+#     point = {key: request.json_body[key] for key in request.json_body}
+#     point_new_id = request.mongo_db.points.insert(point)
+#     return request.mongo_db.points.find_one(point_new_id)
 
 
-@view_config(route_name='point', request_method='PATCH', renderer='json')
-def point_edit_one(request):
-    try:
-        _id = ObjectId(request.matchdict["id"])
-        request.mongo_db.points.find_one(_id)
-    except InvalidId:
-        return HTTPNotFound("There is no such object.")
-    if not isinstance(request.json_body, dict):
-        return HTTPBadRequest("The body of PATCH request must be JSON.")
-    elif not request.json_body:
-        return HTTPBadRequest("The Body of PATCH request is empty. Nothing to do.")
-    else:
-        _id = ObjectId(request.matchdict["id"])
-        request.mongo_db.points.update({"_id": _id},
-                                       {"$set": request.json_body})
-        return request.mongo_db.points.find_one(_id)
+# @view_config(route_name='point', request_method='GET', renderer='json')
+# def point_get_one(request):
+#     try:
+#         _id = ObjectId(request.matchdict["id"])
+#         point = request.mongo_db.points.find_one(_id)
+#         return point
+#     except InvalidId:
+#         return HTTPNotFound("There is no such object")
 
 
-@view_config(route_name='point', request_method='PUT', renderer='json')
-def point_update_one(request):
-    try:
-        _id = ObjectId(request.matchdict["id"])
-        request.mongo_db.points.find_one(_id)
-    except InvalidId:
-        return HTTPNotFound("There is no such object")
-    if not isinstance(request.json_body, dict):
-        return HTTPBadRequest("The body of PUT request must be JSON")
-    elif not request.json_body:
-            return HTTPBadRequest("The body of PUT request is empty. Nothing to do.")
-    else:
-        _id = ObjectId(request.matchdict["id"])
-        replacement = request.json_body.copy()
-        replacement["_id"] = _id
-        request.mongo_db.points.save(replacement)
-        return request.mongo_db.points.find_one(_id)
+# @view_config(route_name='point', request_method='PATCH', renderer='json')
+# def point_edit_one(request):
+#     try:
+#         _id = ObjectId(request.matchdict["id"])
+#         request.mongo_db.points.find_one(_id)
+#     except InvalidId:
+#         return HTTPNotFound("There is no such object.")
+#     if not isinstance(request.json_body, dict):
+#         return HTTPBadRequest("The body of PATCH request must be JSON.")
+#     elif not request.json_body:
+#         return HTTPBadRequest("The Body of PATCH request is empty. Nothing to do.")
+#     else:
+#         _id = ObjectId(request.matchdict["id"])
+#         request.mongo_db.points.update({"_id": _id},
+#                                        {"$set": request.json_body})
+#         return request.mongo_db.points.find_one(_id)
 
 
-@view_config(route_name='point', request_method='DELETE', renderer='json')
-def point_delete_one(request):
-    try:
-        _id = ObjectId(request.matchdict["id"])
-        request.mongo_db.points.remove({"_id": _id})
-        return {}
-    except InvalidId:
-        return HTTPNotFound("There is no such object")
+# @view_config(route_name='point', request_method='PUT', renderer='json')
+# def point_update_one(request):
+#     try:
+#         _id = ObjectId(request.matchdict["id"])
+#         request.mongo_db.points.find_one(_id)
+#     except InvalidId:
+#         return HTTPNotFound("There is no such object")
+#     if not isinstance(request.json_body, dict):
+#         return HTTPBadRequest("The body of PUT request must be JSON")
+#     elif not request.json_body:
+#             return HTTPBadRequest("The body of PUT request is empty. Nothing to do.")
+#     else:
+#         _id = ObjectId(request.matchdict["id"])
+#         replacement = request.json_body.copy()
+#         replacement["_id"] = _id
+#         request.mongo_db.points.save(replacement)
+#         return request.mongo_db.points.find_one(_id)
+
+
+# @view_config(route_name='point', request_method='DELETE', renderer='json')
+# def point_delete_one(request):
+#     try:
+#         _id = ObjectId(request.matchdict["id"])
+#         request.mongo_db.points.remove({"_id": _id})
+#         return {}
+#     except InvalidId:
+#         return HTTPNotFound("There is no such object")
